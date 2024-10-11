2e9d9b07f026cfd0fbec4e6e1e5aa606dc9a8b7c
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-10-11 16:13:08 -0300

    estudando condicionais
    nesse commit realizei um pequeno exercício onde informo para o usuário se o número digitado é impar ou par.

---
+++ aula32_a.py
@@ -0,0 +1,13 @@
+#exercício_1
+numero = int(input("digite um número inteiro: "))
+if numero % 2 == 0:
+    print("O número {} é um número par"\
+          .format(numero))
+else:
+    print("O número {} é um número ímpar"\
+          .format(numero))
+
+
+
+
+
