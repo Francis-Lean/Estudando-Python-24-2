54b8a2024433e8e3beec07c624521cbeae6d4060
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-10-22 15:09:14 -0300

    Add files via upload

---
+++ par_ou_impar.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um número para saber se é par ou impar: "))
+if num % 2 == 0:
+    print("O número {} é um número par".format(num))
+else:
+    print("O número {} é um número impar".format(num))
+
+print("Espero ter ajudado!")
