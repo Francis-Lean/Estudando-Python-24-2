2a53e7583b4c93b0248fd652c5d2368fb85352e9
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-11-19 22:49:59 -0300

    Add files via upload

---
+++ cap4.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um valor: "))
+texto = str(input("Digite uma frase: "))
+
+x = float(numero)
+texto = texto.upper()
+
+print("A conversão do número ficou {}, e da frase ficou {}.".format(x, texto))
